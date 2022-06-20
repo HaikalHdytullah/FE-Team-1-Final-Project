f5c963eefbd3de5b9ccf517cb1f7e66b18b64fb5
Naufal Rizqullah <naufalrm312@gmail.com>
2022-06-20 13:56:27 +0700

    add editproduct

---
+++ src/pages/EditProduct.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavbarInfoProduct from "../components/NavbarInfoProduct";
+import EditProduk from "../components/EditProduk";
+
+export default function InfoProduct() {
+  return (
+    <>
+      <NavbarInfoProduct />
+      <EditProduk />
+    </>
+  );
+}
