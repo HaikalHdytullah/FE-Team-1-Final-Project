3a81554a3a3037ec788549bd61b47135e7730510
Fajar Satrio Utomo <fajarsatrio2@gmail.com>
2022-06-27 19:39:02 +0700

    perbaikan bug

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,18 +1,14 @@
 import Header from "../components/Header";
-import Diskon from "../components/Diskon";
+import BannerSection from "../components/HomepageCarousel";
 import Kartu from "../components/Kartu";
 import Kategori from "../components/kategori";
-import Slider from "../components/Slider";
+// import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <>
       <Header />
-      <div className="flex-warp mt-10 flex w-full items-center justify-center">
-        <section className="h-100 gradient-form">
-          <Slider />
-        </section>
-      </div>
+      <BannerSection />
       {/* <Diskon /> */}
       <Kategori />
       <Kartu />
