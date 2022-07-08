935cf4cf988a1bae2918963b8d544d5370a8f182
Haikal Hidayatullah <haikal.hidayat.2103@gmail.com>
2022-07-08 16:53:53 +0700

    fix fix fix

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,7 +1,7 @@
 import Header from "../components/Header";
 import BannerSection from "../components/HomepageCarousel";
 import Kartu from "../components/Kartu";
-import Kategori from "../components/Kategori.jsx";
+import Kategori from "../components/kategori";
 import Footer from "../components/Footer";
 
 const Home = () => {
